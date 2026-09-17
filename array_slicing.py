51bee18f4c009ca71cf4469cafaad82f66c37d30
Diya Nagpure <diyanagpure46@gmail.com>
2026-09-17 22:09:24 +0530

    Add NumPy array slicing program

---
+++ array_slicing.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("First three elements:", arr[0:3])
+print("Last three elements:", arr[2:5])
+print("Every second element:", arr[::2])
